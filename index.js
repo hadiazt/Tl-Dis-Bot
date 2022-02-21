4a190c2fe9dc120b4ab33b8c915e4094b393e4cb
hadi <65118892+hadiazt@users.noreply.github.com>
2022-02-21 19:36:26 +0330

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const setting = new Database('./data/config.json')
 var TL_token = setting.get('tltoken');
 var DIS_token = setting.get('distoken');
 var LANG = setting.get('general').lang; // KR / EN / IR
+var CHANNEL = setting.get('general').DiscordChannelID
 
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf(TL_token)
@@ -16,3 +17,32 @@ client.login(DIS_token)
 bot.start((ctx) => ctx.reply(setting.get('general').start))
 bot.help((ctx) => ctx.reply(setting.get('general').help))
 
+bot.on('message', (ctx) => {
+
+    const message = ctx.update.message.text;
+    const authorID = ctx.update.message.from.id;
+    const authorUSERNAME = ctx.update.message.from.username;
+    const photo = ctx.update.message.photo[ctx.update.message.photo.length - 1].file_id;
+
+    if (!LANG) {
+        ctx.reply(setting.get('general').langerr)
+        if (!DiscordChannelID) {
+            ctx.reply(setting.get('general').langerr)
+        } else {
+            if (message && photo) {
+                if (LANG === 'KR') {
+
+                    client.channels.cache.get(CHANNEL).send()
+                } else if (LANG === 'EN') {
+
+                    client.channels.cache.get(CHANNEL).send()
+                } else if (LANG === 'IR') {
+
+                    client.channels.cache.get(CHANNEL).send()
+                }
+            }
+        }
+    } else {
+        ctx.reply(setting.get('general').langerr)
+    }
+})
