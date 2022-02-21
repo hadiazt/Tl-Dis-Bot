c1b01c87979e80973dc02adebb0c73c0eaaa68fb
hadi <65118892+hadiazt@users.noreply.github.com>
2022-02-21 13:12:17 +0330

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,23 @@ var langconfig = setting.get('general')
 
 
 const { Telegraf } = require('telegraf')
-const tlbot = new Telegraf(telegram.token)
-tlbot.launch()
+const bot = new Telegraf(telegram.token)
+bot.launch()
 
 const { Client } = require('discord.js')
 const client = new Client()
 client.login(dis.token)
+
+// TELEGRAM
+bot.start((ctx) => ctx.reply(''))
+bot.help((ctx) => ctx.reply(''))
+
+
+bot.on('text', (ctx) => {
+    const message = ctx.message.text
+    const author = ctx.message.from.id || ctx.message.from.username
+
+    if (message && author === '566753238' || 'hadiazt') {
+        console.log(message);
+    }
+})
