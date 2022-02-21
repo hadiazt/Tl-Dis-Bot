fafcbbceb3afa17e167fdf7e127d3d34dfa22498
hadi <65118892+hadiazt@users.noreply.github.com>
2022-02-21 11:17:10 +0330

    Setting Telegram Bot

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const { Database } = require('beta.db')
 const setting = new Database('./data/config.json')
+const language = new Database('./lang/config.json')
+
 var telegram = setting.get('telegram')
 var dis = setting.get('discord')
-var lang = setting.get('general')
+var langconfig = setting.get('general')
 
 
+const { Telegraf } = require('telegraf')
+const tlbot = new Telegraf(telegram.token)
+tlbot.launch()
 
